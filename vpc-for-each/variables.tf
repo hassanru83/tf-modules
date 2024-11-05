c57b7238e4980a4238b0d91ea154085ed070001e
hassanru83 <114787442+hassanru83@users.noreply.github.com>
2024-11-04 21:45:25 -0500

    Update variables.tf

--- vpc-for-each/variables.tf
+++ vpc-for-each/variables.tf
@@ -5,6 +5,6 @@ variable "vpc_objects" {
     routing_mode = string
     description = string
     delete_default_routes_on_create = bool
-    auto_create_subnetworks = true
+    auto_create_subnetworks = bool
   }))
 }
